fcddefefd06486fbf7b6427f6300f0921854e9c9
Yuliuus <yuliiamarkovska@gmail.com>
2024-11-10 23:20:16 +0200

    reviews section update

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,8 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getCamperById } from "../../redux/selectors";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchCamperById } from "../../redux/operations";
+
 export default function Reviews() {
+  const dispatch = useDispatch();
+  const camper = useSelector(getCamperById);
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchCamperById(id)); // Dispatch the action to fetch data
+    }
+  }, [id, dispatch]);
+
+  if (!camper) return <p>Loading...</p>;
+
+  const { reviews } = camper;
+  console.log(camper);
+
   return (
     <div>
-      <img src="" alt="" />
-      <h2>reviews</h2>
+      <ul>
+        {reviews.map((review, index) => (
+          <li key={index}>
+            <h2>{review.reviewer_name}</h2>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
